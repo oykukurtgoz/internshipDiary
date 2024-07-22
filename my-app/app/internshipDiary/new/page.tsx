1e50f7405916d11e1d72f53ed7c3cf2112254b61
Oyku <oykukurtgoz@gmail.com>
2024-07-22 13:34:00 +0300

    added markdown editor

--- my-app/app/internshipDiary/new/page.tsx
+++ my-app/app/internshipDiary/new/page.tsx
@@ -1,13 +1,16 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+"use client";
+import { Button, TextField } from '@radix-ui/themes'
 
-import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
 
 const newDiaryPage = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder= 'Title'></TextField.Root>
-      <TextArea placeholder="Description" />
-      <Button>Submit new Diary</Button>
+      <SimpleMDE placeholder="Description" />
+      <Button>Save new Diary</Button>
     </div>
   )
 }
